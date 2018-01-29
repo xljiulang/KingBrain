077856417489561050ded02ca1bd61721d611e4e
xljiulang <366193849@qq.com>
2018-01-29 10:14:32 +0800

    index

--- KingQuestionProxy/HomeController.cs
+++ KingQuestionProxy/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using NetworkSocket.Http;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,11 @@ namespace KingQuestionProxy
     public class HomeController : HttpController
     {
         /// <summary>
+        /// 代理端口
+        /// </summary>
+        private static readonly string proxyPort = ConfigurationManager.AppSettings["ProxyPort"];
+
+        /// <summary>
         /// 首页
         /// </summary>
         /// <returns></returns>
@@ -20,6 +26,12 @@ namespace KingQuestionProxy
         public ActionResult Index()
         {
             var html = System.IO.File.ReadAllText("index.html", Encoding.UTF8);
+            var model = new IndexModel
+            {
+                cerhref = $"http://{this.Request.Url.Host}:{proxyPort}/fiddlerRoot.cer",
+                proxyIpEndpoint = $"{this.Request.Url.Host}:{proxyPort}"
+            };
+            html = html.Replace("@cerhref", model.cerhref).Replace("@proxyIpEndpoint", model.proxyIpEndpoint);
             return Content(html);
         }
     }
---
+++ KingQuestionProxy/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingQuestionProxy
+{
+    class IndexModel
+    {
+        public string cerhref { get; set; }
+
+        public string proxyIpEndpoint { get; set; }
+    }
+}
