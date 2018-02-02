447ef57b29a3d2daca69472fe4cf8baeed11954d
xljiulang <366193849@qq.com>
2018-02-02 19:51:38 +0800

    多搜索引擎加入，正确率从72%提升到77%

--- KingQuestionProxy/Program.cs
+++ KingQuestionProxy/Program.cs
@@ -7,20 +7,49 @@ namespace KingQuestionProxy
 {
     public class Program
     {
+        static long total = 0L;
+        static long ok = 0L;
+        static object root = new object();
         public static void Main(string[] args)
         {
             var logger = new Logger(Console.Out, new DebugWriter());
             Console.SetOut(logger);
 
-            //var e = new LiZhiSearchEngine();
-            //var f = e.Search(new KingQuestion
-            //{
-            //    data = new KingQuestionData
-            //    {
-            //        quiz = "标准钢琴白键有多少个",
-            //        options = new[] { "51", "52", "53", "54" }
-            //    }
-            //});
+
+            System.Net.ServicePointManager.DefaultConnectionLimit = 50;
+
+            var sqlLite = new SqlliteContext();
+            var ran = new Random();
+            var datas = sqlLite.QuizAnswer.ToArray().OrderBy(item => ran.Next()).ToArray();
+            datas.AsParallel().ForAll(item =>
+            {
+                System.Threading.Interlocked.Increment(ref total);
+                var question = new KingQuestion
+                {
+                    data = new KingQuestionData
+                    {
+                        quiz = item.Quiz,
+                        options = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(item.OptionsJson),
+                    }
+                };
+                var best = Searcher.Search(question);
+                if (best?.Option == item.Answer)
+                {
+                    System.Threading.Interlocked.Increment(ref ok);
+                }
+
+                lock (root)
+                {
+                    var cuect = ((double)ok / (double)total * 100d).ToString("0.00");
+                    var pecent = ((double)total / (double)datas.Length * 100d).ToString("0.00");
+                    Console.WriteLine(item.Quiz);
+                    Console.WriteLine($"猜测答案：{best?.Option}");
+                    Console.WriteLine($"正确答案：{item.Answer}");
+                    Console.Title = $"进度：{pecent}%  准确率：{cuect}%";
+                    Console.WriteLine();
+                }
+            });
+
 
             HostFactory.Run(x =>
             {
